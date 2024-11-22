367ee651bd49fc95102cdbce9355d3afd08305df
Oliver Mak <oliver139.working@gmail.com>
2024-11-22 23:49:56 +0800

    Initialize project

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,7 @@
 import { Icon } from "@iconify/vue";
-import { createPinia } from "pinia";
 import { createApp } from "vue";
 import pluginVueGtag from "vue-gtag";
 import App from "./App.vue";
-import i18n from "./i18n";
-import router from "./router";
 
 // #region : CSS
 import "normalize.css";
@@ -15,11 +12,6 @@ import "@/assets/css/main.scss";
 
 const app = createApp(App);
 
-// Vue Plugins
-app.use(createPinia());
-app.use(router);
-app.use(i18n);
-
 // Global Component
 app.component("Icon", Icon);
 
@@ -28,7 +20,7 @@ app.use(pluginVueGtag, {
   config: {
     id: "###", // Change to yout Google Analytics ID
   },
-}, router);
+});
 
 // Mount it
 app.mount("#app");
