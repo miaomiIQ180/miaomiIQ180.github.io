29863c63e3119289b968824e77b84b1ee6667a99
Oliver Mak <oliver.mak@naxos.com>
2024-11-29 17:54:53 +0800

    Button style

--- src/App.vue
+++ src/App.vue
@@ -12,5 +12,55 @@ const { iq } = storeToRefs(useAppStore());
 </script>
 
 <style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+  height: inherit;
+  margin: 1rem 1.5rem;
+  > h1 {
+    color: #903862;
+    font-size: 1.75rem;
+    text-align: center;
+    margin: 0;
+  }
+}
 
+.current-iq, .hairpin {
+  color: rgb(var(--iq-text));
+  font-size: 1.625rem;
+  font-weight: bold;
+  text-align: center;
+  line-height: 1;
+  -webkit-text-stroke: 1px rgb(var(--iq-border));
+}
+.current-iq {
+  font-size: 3.625rem;
+  text-align: center;
+  -webkit-text-stroke: 2px rgb(var(--iq-border));
+  margin: 1.5rem 0 2rem;
+}
+.hairpin {
+  position: absolute;
+  transform: translate(-50%, -50%) rotate(-10deg);
+  text-shadow:
+    0 0 .125rem #fff,
+    0 0 .125rem #fff,
+    0 0 .125rem #fff,
+    0 0 .125rem #fff,
+    0 0 .125rem #fff,
+    0 0 .125rem #fff,
+    0 0 .125rem #fff,
+    0 0 .125rem #fff,
+    0 0 .125rem #fff,
+    0 0 .125rem #fff;
+  left: 38%;
+}
+
+.social-accounts {
+  position: fixed;
+  bottom: 1.25rem;
+  right: 1rem;
+}
 </style>
--- src/components/ButtonArea.vue
+++ src/components/ButtonArea.vue
@@ -3,7 +3,11 @@
     class="button-area"
     :class="{ pressing: isPressed }"
   >
-    <button ref="plusIqBtn" type="button" @click="addIq()">
+    <button
+      ref="plusIqBtn"
+      type="button"
+      class="plus-iq"
+    >
       <template v-if="isPressed">Plusing 1</template>
       <template v-else>Plus one</template>
     </button>
@@ -21,5 +25,35 @@ whenever(() => !isPressed.value, () => {
 </script>
 
 <style lang="scss" scoped>
+.button-area {
+  --btn-size: 15rem;
+  text-align: center;
+  width: 100%;
+}
 
+.plus-iq {
+  display: flex;
+  position: relative;
+  justify-content: center;
+  align-items: center;
+  width: var(--btn-size);
+  height: var(--btn-size);
+  overflow: hidden;
+  background: #e9e9ed;
+  border: none;
+  border-radius: var(--btn-size);
+  padding: 0;
+  transition: all .15s ease;
+  -webkit-tap-highlight-color: transparent;
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  img {
+    pointer-events: none;
+  }
+  &.pressed {
+    padding-top: 1rem;
+  }
+  &:hover {
+    background: rgb(var(--color-theme4));
+  }
+}
 </style>
