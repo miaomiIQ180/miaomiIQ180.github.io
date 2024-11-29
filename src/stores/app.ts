6b961df60434c220394f03aec83a89173d0a4b30
Oliver Mak <oliver139.working@gmail.com>
2024-11-29 23:33:46 +0800

    Play sound on add IQ

--- src/stores/app.ts
+++ src/stores/app.ts
@@ -1,9 +1,12 @@
+import kirby1up from "@/assets/sound/kirby-1up.mp3";
 import { defineStore } from "pinia";
 
 export const useAppStore = defineStore("app", () => {
   const iq = useStorage("iq", 0);
 
+  const { play: play1up } = useSound(kirby1up);
   const addIq = useDebounceFn((n: number = 1) => {
+    play1up();
     iq.value += n;
   }, 50);
 
