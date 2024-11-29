a6677f57c9170e3043a9c137ed95e6efcac02504
Oliver Mak <oliver.mak@naxos.com>
2024-11-29 16:35:44 +0800

    Plus IQ function

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,14 @@
   <div class="container">
     <h1>喵咪現在是</h1>
     <p class="current-iq">IQ{{ iq }}</p>
+    <button type="button" @click="addIq()">Plus one</button>
   </div>
 </template>
 
 <script setup lang="ts">
-const { iq } = storeToRefs(useAppStore());
+const appStore = useAppStore();
+const { iq } = storeToRefs(appStore);
+const { addIq } = appStore;
 </script>
 
 <style lang="scss" scoped>
--- src/stores/app.ts
+++ src/stores/app.ts
@@ -3,5 +3,13 @@ import { defineStore } from "pinia";
 export const useAppStore = defineStore("app", () => {
   const iq = useStorage("iq", 0);
 
-  return { iq };
+  function addIq(n: number = 1) {
+    iq.value += n;
+  }
+
+  function setIq(n: number) {
+    iq.value = n;
+  }
+
+  return { iq, addIq, setIq };
 });
